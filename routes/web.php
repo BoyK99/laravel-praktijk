c80cfc5f3edc8630dd5e7f591317d1ed100bf1f2
BoyK99 <dieren1234@hotmail.com>
2022-10-06 01:02:50 +0200

    make:view dependency routes

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+//    return view('welcome');
+//    return "hello world";
+//    return  ['foo' =>  'bar'];
+//    return view('data');
+    return view('posts');
 });
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('post', function () {
+    return view('post');
+});
+
+
 
 Auth::routes();
 
